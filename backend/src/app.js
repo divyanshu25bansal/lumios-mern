7398e763c5bf8e6efcc2a97066465a6ed353852d
divyanshu25bansal <divyanshubansal025@gmail.com>
2026-06-17 00:02:37 +0530

    authentication api added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/dbConfig.js";
 import authRouter from "./routes/authRouter.js";
 import sleepRouter from "./routes/sleepRouter.js";
@@ -13,8 +14,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
 app.use("/", authRouter);
